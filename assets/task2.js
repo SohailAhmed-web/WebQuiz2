5ed322c6ec796d592178655e0490d33116bcc694
SohailAhmed-web <sohail.bscsf19@iba-suk.edu.pk>
2023-09-27 13:10:44 +0500

    Task2 Done

---
+++ assets/task2.js
@@ -0,0 +1,22 @@
+const persons = [
+    {
+
+        id: 1,
+		personName: "Fahad",
+		personAge: "23",
+		personIncome: "2000000"
+    },
+    {
+        id: 2,
+		personName: "Zoya",
+		personAge: "23",
+		personIncome: "3000000"
+    },
+   
+];
+
+for (let i = 0; i < persons.length; i++) {
+    const person = persons[i];
+    console.log(`Person ${i + 1} name is ${person.personName} having salary ${person.personIncome},`);
+}
+
